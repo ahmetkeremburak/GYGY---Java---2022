9537e7fb537ac4ae8c762fd95caae757d55d150d
Kerem <Kerem@DESKTOP-BSBJBQM>
2022-10-09 16:46:31 +0300

    Challenge 1 | Another solution using switch case structure

--- Challenge_1/src/For_ve_while_ile.java
+++ Challenge_1/src/For_ve_while_ile.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class For_ve_while_ile {
 
 	public static void main(String[] args) {
-		// This code is to replace all vowels in a string with appropriate numbers.
-		//Starting from "A" with "1", "E" with "2", so on and so forth.
+//		 This code is to replace all vowels in a string with appropriate numbers.
+//		Starting from "A" with "1", "E" with "2", so on and so forth.
 		Scanner scn = new Scanner(System.in);
 		System.out.print("Adınızı bahşeder misiniz: ");
 		String isim = scn.nextLine();
@@ -15,6 +15,10 @@ public class For_ve_while_ile {
 	}
 
 	public static String changing_vowels(String isim) {
+//		while-loop and for-loop goes over every character of the input string
+//		compares every character to characters in harfler[] char array
+//		If condition replaces every matching character with their matching character in numaralar[] at the same index.
+//		Output is written to a StringBuilder object and returned by transforming it to string.
 		StringBuilder isim2 = new StringBuilder(isim);
 		char[] harfler = {'a','A','e','E','ı','I','i','İ','o','O','ö','Ö','u','U','ü','Ü'};
 		char[] numaralar = {'1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8'};
--- Challenge_1/src/If_ve_Switch_Case.java
+++ Challenge_1/src/If_ve_Switch_Case.java
@@ -3,26 +3,94 @@ import java.util.Scanner;
 public class If_ve_Switch_Case {
 
 	public static void main(String[] args) {
-		// This code is to replace all vowels in a string with appropriate numbers.
-		//Starting from "A" with "1", "E" with "2", so on and so forth.
+//		 This code is to replace all vowels in a string with appropriate numbers.
+//		Starting from "A" with "1", "E" with "2", so on and so forth.
 		Scanner scn = new Scanner(System.in);
 		System.out.print("Adınızı bahşeder misiniz: ");
 		String isim = scn.nextLine();
-		isim = isim.replace( 'a', '1' );
-		isim = isim.replace( 'e', '2' );
-		isim = isim.replace( 'ı', '3' );
-		isim = isim.replace( 'i', '4' );
-		isim = isim.replace( 'o', '4' );
-		isim = isim.replace('ö', '5');
-		isim= isim.replace( 'u', '5' );
-		System.out.println( isim );
+		String isim2 = changing_vowels(isim);
+		System.out.println(isim2);
+		scn.close();
 
 	}
-	
-	
-	public static void changing_vowels(String isim) {
-		
+
+	private static String changing_vowels(String isim) {
+//		For-loop iterates every character in given string variable
+//		Switch-Case structure builds a new string according to given cases
+//		StringBuilder object is transformed to String and returned.
+		StringBuilder isim2 = new StringBuilder();
+		for (int i = 0; i < isim.length(); i++) {
+			switch (isim.charAt(i)) {
+			case 'a':
+				isim2.append(1);
+				break;
+				
+			case 'A':
+				isim2.append(1);
+				break;
+				
+			case 'e':
+				isim2.append(2);
+				break;
+				
+			case 'E':
+				isim2.append(2);
+				break;
+				
+			case 'I':
+				isim2.append(3);
+				break;	
+			
+			case 'ı':
+				isim2.append(3);
+				break;
+				
+			case 'İ':
+				isim2.append(4);
+				break;
+				
+			case 'i':
+				isim2.append(4);
+				break;
+				
+			case 'O':
+				isim2.append(5);
+				break;
+			
+			case 'o':
+				isim2.append(5);
+				break;
+				
+			case 'ö':
+				isim2.append(6);
+				break;
+				
+			case 'Ö':
+				isim2.append(6);
+				break;
+				
+			case 'u':
+				isim2.append(7);
+				break;
+				
+			case 'U':
+				isim2.append(7);
+				break;
+				
+			case 'ü':
+				isim2.append(8);
+				break;
+				
+			case 'Ü':
+				isim2.append(8);
+				break;
+				
+			default:
+				isim2.append(isim.charAt(i));
+				break;
+			}
 		}
+		return isim2.toString();
 	}
 
-
+}
