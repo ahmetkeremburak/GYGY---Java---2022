a93dd77ee54041fcb31d13c3ebfb57487cac39e2
ahmetkeremburak <keremburak00@gmail.com>
2022-10-11 23:42:52 +0300

    Add | Simple calculator and its .jar build

---
+++ Simple Calculator/src/kerem/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package kerem.calculator;
+
+public class Calculator {
+	
+	public double add(double x, double y) {
+		return x + y;
+	}
+	
+	public double substract(double x, double y) {
+		return x - y;
+	}
+	
+	public double multiply(double x, double y) {
+		return x * y;
+	}
+	
+	public double divide(double x, double y) {
+		return x / y;
+	}
+	
+}
